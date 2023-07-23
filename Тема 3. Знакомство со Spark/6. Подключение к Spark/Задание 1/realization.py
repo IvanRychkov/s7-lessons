e1a8aa5853178d32096b00410e524789584a7590
Ivan Rychkov <rychkov.ivan@gmail.com>
2023-07-23 16:15:27 +0300

    spark connection done

--- Тема 3. Знакомство со Spark/6. Подключение к Spark/Задание 1/realization.py
+++ Тема 3. Знакомство со Spark/6. Подключение к Spark/Задание 1/realization.py
@@ -1,2 +1,8 @@
 from pyspark.sql import SparkSession
-spark = # дополните код
+
+# Поделючение с конфигурацией
+spark = SparkSession.builder \
+    .config('spark.driver.memory', '1g') \
+    .config('spark.driver.cores', 2) \
+    .appName('My first session') \
+    .getOrCreate()
--- Тема 3. Знакомство со Spark/6. Подключение к Spark/Задание 2/realization.py
+++ Тема 3. Знакомство со Spark/6. Подключение к Spark/Задание 2/realization.py
@@ -1,2 +1,7 @@
 from pyspark.sql import SparkSession
-spark = # дополните код
+
+# Подключение с выбором режима работы (master)
+spark = SparkSession.builder \
+    .master('yarn') \
+    .appName('My second session') \
+    .getOrCreate()
